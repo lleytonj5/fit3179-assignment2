c08a73937ff0e8e177b625b776bcb31b30835031
lleytonj5 <86594328+lleytonj5@users.noreply.github.com>
2021-10-14 12:04:27 +1100

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -2,7 +2,7 @@
 // vegaEmbed("#bar_chart", vg_1).then(function(result) {
 //     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 //     }).catch(console.error);
-var vg_2 = "electricity_map.vg.json";
+var vg_2 = "electricity_consumption_map.vg.json";
 vegaEmbed("#electricity_map", vg_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
     }).catch(console.error);
