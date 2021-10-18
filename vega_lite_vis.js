c8234d845997a08252fb82e14fcfaf69ed530bcc
lleytonj5 <86594328+lleytonj5@users.noreply.github.com>
2021-10-18 17:08:37 +1100

    Update visualisation files

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -16,3 +16,13 @@ var vg_4 = "electricity_imports_map.vg.json";
 vegaEmbed("#electricity_map_3", vg_4).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
     }).catch(console.error);
+
+var vg_5 = "electricity_renewables_map.vg.json";
+vegaEmbed("#electricity_map_4", vg_5).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+
+// Function to add the % sign to the tooltip in the renewable electricity map
+vega.expressionFunction('percentageFormat', function(datum, params) {
+    return datum + '%';
+    });
